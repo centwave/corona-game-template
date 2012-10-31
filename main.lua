1c6301dc4a462ac05346c6b7376e4bf0c157933b
Centwave <centwave@gmail.com>
2012-10-31 20:39:06 +0800

    for sublime text IDE, add a code for print message in the console

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+-- for sublime text IDE, we need to add following code
+-- in order to show the print message in the console
+io.output():setvbuf("no")
+
 display.setStatusBar( display.HiddenStatusBar )
 
 -- require controller module
